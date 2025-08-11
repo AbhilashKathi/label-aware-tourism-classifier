a45faa002b79ca7c50a4ddb9e2155cc165cdaaf4
Abhilash Kathi <abhilashkathi1@gmail.com>
2025-08-11 13:05:27 +0530

    fresh start

--- data/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("wiki_movie_plots_deduped.csv")
-df  = df.dropna(subset=['Plot','Genre'])
-df = df[df['Genre'].str.lower()!= 'unknown']
-print(df[['Plot','Genre']].head())
-print(f"\nTotal usable rows: {len(df)}")
